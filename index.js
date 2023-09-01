02411b878442246b8cb98841e0499bed9ead2fa7
chienlv <chienlv.tb@outlook.com>
2023-09-01 18:23:39 +0700

    f2

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const addToken = require('./fetch_data')
+const fs = require('fs')
 
 app.use(express.static(__dirname + '/public'));
 app.set('view engine','ejs');
@@ -9,7 +10,13 @@ app.get('/', function (req, res) {
   res.render('home')
 })
 app.get('/list-token', function (req, res) {
-    res.json({tokens: ['dydx','op','celo','flux','xrp']})
+  fs.readdir('./public/chart_data', (err, files) => {
+    var tokens = []
+    files.forEach(file => {
+      tokens.push(file.split('.')[0])
+    });
+    res.json({tokens: tokens})
+  });
 })
 app.get('/add-token', function(req, res) {
     var params = req.query
